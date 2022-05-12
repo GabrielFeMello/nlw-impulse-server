4c1baa37fc63eae57fb701545c45d4b05448233d
Gabriel Brother <gabriel.fe.mello@gmail.com>
2022-05-12 05:56:23 -0300

    deploy PORT

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ app.use(cors({ origin: "http://localhost:3003" }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP Server Running");
 });
